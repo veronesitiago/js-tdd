e492a3f8018816f71590602c17f05c8e6544eeed
tiago <tiagoveronesi@outcenter.com.br>
2019-12-27 11:00:52 -0300

    Mocha: describe, context e it

---
+++ tests/main.spec.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+describe('Main', () => {
+  context('Case 1', () => {
+    describe('Method A', () => {
+      it('should happen blablabla', () => {
+        // espera que aconteça
+        // entrada de dados / método sum(2,2)
+        // espera retornar (4) => true | (3) => false => broken test
+      });
+    });
+  });
+});

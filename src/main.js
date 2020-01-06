c0b31bc5dd291da919c06c14855dcee0df43e6bf
tiago <tiagoveronesi@outcenter.com.br>
2020-01-06 18:38:31 -0300

    FizzBuzz com tdd

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-const sum = (num1, num2) => num1 + num2;
-const sub = (num1, num2) => num1 - num2;
-const mult = (num1, num2) => num1 * num2;
-const div = (num1, num2) => ((num2 === 0) ? 'Não é possível divisão por zero!' : num1 / num2);
+const FizzBuzz = (num) => {
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
+  return num;
+};
 
-export { sum, sub, mult, div }
+export default FizzBuzz;

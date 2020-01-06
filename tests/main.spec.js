6bce2b71e960e33c88a8f5257e2a7cb0745d3994
tiago <tiagoveronesi@outcenter.com.br>
2020-01-06 18:14:54 -0300

    Testes Calc

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,52 +1,62 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
+import { expect } from 'chai';
+import { sum, sub, mult, div } from '../src/main';
 
-let expect = require('chai').expect;
+describe('Calc', () => {
+  // smokes tests
+  describe('Smokes tests', () => {
+    it('should exist method `sum`', () => {
+      expect(sum).to.exist;
+      expect(sum).to.be.a('function');
+    });
 
-describe('Main', () => {
-  let arr;
-  // Roda uma vez antes do bloco
-  before(() => {
-    // console.log('before');
+    it('should exist method `sub`', () => {
+      expect(sub).to.exist;
+      expect(sub).to.be.a('function');
+    });
+
+    it('should exist method `mult`', () => {
+      expect(mult).to.exist;
+      expect(mult).to.be.a('function');
+    });
+
+    it('should exist method `div`', () => {
+      expect(div).to.exist;
+      expect(div).to.be.a('function');
+    });
   });
 
-  // Roda uma vez depois do bloco
-  after(() => {
-    // console.log('after');
+  describe('Sum', () => {
+    it('should return 4 when `sum(2, 2)`', () => {
+      expect(sum(2, 2)).to.be.equal(4);
+    });
   });
 
-  // Roda todas as vezes, antes de cada bloco
-  beforeEach(() => {
-    // console.log('beforeEach');
-    arr = [1, 2, 3];
+  describe('Sub', () => {
+    it('should return 4 when `sub(6, 2)`', () => {
+      expect(sub(6, 2)).to.be.equal(4);
+    });
+
+    it('should return -4 when `sub(6, 10)`', () => {
+      expect(sub(6, 10)).to.be.equal(-4);
+    });
   });
 
-  // Roda todas as vezes, depois de cada bloco
-  afterEach(() => {
-    // console.log('afterEach');
+  describe('Mult', () => {
+    it('should return 4 when `mult(2, 2)`', () => {
+      expect(mult(2, 2)).to.be.equal(4);
+    });
   });
 
-  it('should be an array', () => {
-    expect(arr).to.be.a('array');
-  });
+  describe('div', () => {
+    it('should return 4 when `div(8, 2)`', () => {
+      expect(div(8, 2)).to.be.equal(4);
+    });
 
-  it('should have a size of 4 when push another value to the array', () => {
-    arr.push(4);
-    expect(arr).to.have.lengthOf(4);
-  });
-
-  it('should remove the value 3 when use pop in the array', () => {
-    arr.pop();
-    expect(arr).to.not.include(3);
-  });
-
-  it('should return true when pop 3 from the array', () => {
-    expect(arr.pop() === 3).to.be.true;
-  });
-
-  it('should have a size of 2 when pop a value from the array', () => {
-    arr.pop();
-    expect(arr).to.have.lengthOf(2);
+    it('should return `não é possível divisão por zero!` when divide by zero', () => {
+      expect(div(4, 0)).to.be.equal('Não é possível divisão por zero!');
+    });
   });
 });
